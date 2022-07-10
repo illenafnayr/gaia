e815792dc1f92736a625280bf2ebbde7df60f634
RyanFanelli <ryan.fanelli@greenmountainpower.com>
2022-07-10 16:47:57 -0400

    set w1 gpio pin to 17 for ds18b29

--- sensor-scripts/run_ds18B20.py
+++ sensor-scripts/run_ds18B20.py
@@ -3,7 +3,11 @@ import glob
 import time
 from datetime import datetime
 import csv
-import keyboard
+import RPi.GPIO as GPIO
+
+GPIO_PIN_NUMBER=17
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(GPIO_PIN_NUMBER, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 file = open("../ds18B20.csv", "w", newline="")
 csv = csv.writer(file)

1a68a8e70508eded4283fc9822a56dc3e6571997
RyanFanelli <ryan.fanelli@greenmountainpower.com>
2022-07-13 21:28:01 -0400

    update run_dht11.c

--- sensor-scripts/run_dht11.c
+++ sensor-scripts/run_dht11.c
@@ -1,64 +1,76 @@
-#include <wiringPi.h>  
-#include <stdio.h>  
-#include <stdlib.h>  
-#include <stdint.h>  
-#define MAX_TIME 85  
-#define DHT11PIN 5  
-int dht11_val[5]={0,0,0,0,0};  
-
-void dht11_read_val()  
-{  
-  uint8_t lststate=HIGH;  
-  uint8_t counter=0;  
-  uint8_t j=0,i;  
-  float farenheit;  
-  for(i=0;i<5;i++)  
-     dht11_val[i]=0;  
-  pinMode(DHT11PIN,OUTPUT);  
-  digitalWrite(DHT11PIN,LOW);  
-  delay(18);  
-  digitalWrite(DHT11PIN,HIGH);  
-  delayMicroseconds(40);  
-  pinMode(DHT11PIN,INPUT);  
-  for(i=0;i<MAX_TIME;i++)  
-  {  
-    counter=0;  
-    while(digitalRead(DHT11PIN)==lststate){  
-      counter++;  
-      delayMicroseconds(1);  
-      if(counter==255)  
-        break;  
-    }  
-    lststate=digitalRead(DHT11PIN);  
-    if(counter==255)  
-       break;  
-    // top 3 transistions are ignored  
-    if((i>=4)&&(i%2==0)){  
-      dht11_val[j/8]<<=1;  
-      if(counter>16)  
-        dht11_val[j/8]|=1;  
-      j++;  
-    }  
-  }  
-  // verify cheksum and print the verified data  
-  if((j>=40)&&(dht11_val[4]==((dht11_val[0]+dht11_val[1]+dht11_val[2]+dht11_val[3])& 0xFF)))  
-  {  
-    farenheit=dht11_val[2]*9./5.+32;  
-    printf("Humidity = %d.%d %% Temperature = %d.%d *C (%.1f *F)\n",dht11_val[0],dht11_val[1],dht11_val[2],dht11_val[3],farenheit);  
-  }  
-  else  
-    printf("Invalid Data!!\n");  
-}  
-
-int main(void)  
-{  
-  printf("Interfacing Temperature and Humidity Sensor (DHT11) With Raspberry Pi\n");  
-  if(wiringPiSetup()==-1)  
-    exit(1);  
-  while(1)  
-  {  
-     dht11_read_val();  
-     delay(3000);  
-  }  
-  return 0;  
+#include <wiringPi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#define MAXTIMINGS	85
+#define DHTPIN		11
+int dht11_dat[5] = { 0, 0, 0, 0, 0 };
+ 
+void read_dht11_dat()
+{
+	uint8_t laststate	= HIGH;
+	uint8_t counter		= 0;
+	uint8_t j		= 0, i;
+	float	f; 
+ 
+	dht11_dat[0] = dht11_dat[1] = dht11_dat[2] = dht11_dat[3] = dht11_dat[4] = 0;
+ 
+	pinMode( DHTPIN, OUTPUT );
+	digitalWrite( DHTPIN, LOW );
+	delay( 18 );
+	digitalWrite( DHTPIN, HIGH );
+	delayMicroseconds( 40 );
+	pinMode( DHTPIN, INPUT );
+ 
+	for ( i = 0; i < MAXTIMINGS; i++ )
+	{
+		counter = 0;
+		while ( digitalRead( DHTPIN ) == laststate )
+		{
+			counter++;
+			delayMicroseconds( 1 );
+			if ( counter == 255 )
+			{
+				break;
+			}
+		}
+		laststate = digitalRead( DHTPIN );
+ 
+		if ( counter == 255 )
+			break;
+ 
+		if ( (i >= 4) && (i % 2 == 0) )
+		{
+			dht11_dat[j / 8] <<= 1;
+			if ( counter > 16 )
+				dht11_dat[j / 8] |= 1;
+			j++;
+		}
+	}
+ 
+	if ( (j >= 40) &&
+	     (dht11_dat[4] == ( (dht11_dat[0] + dht11_dat[1] + dht11_dat[2] + dht11_dat[3]) & 0xFF) ) )
+	{
+		f = dht11_dat[2] * 9. / 5. + 32;
+		printf( "Humidity = %d.%d %% Temperature = %d.%d C (%.1f F)\n",
+			dht11_dat[0], dht11_dat[1], dht11_dat[2], dht11_dat[3], f );
+	}else  {
+		printf( "Data not good, skip\n" );
+	}
+}
+ 
+int main( void )
+{
+	printf( "Raspberry Pi wiringPi DHT11 Temperature test program\n" );
+ 
+	if ( wiringPiSetup() == -1 )
+		exit( 1 );
+ 
+	while ( 1 )
+	{
+		read_dht11_dat();
+		delay( 1000 ); 
+	}
+ 
+	return(0);
 }

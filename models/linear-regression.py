9699ab2d851c51d0487226ff8128856681314e08
RyanFanelli <ryan.fanelli@greenmountainpower.com>
2022-07-18 22:18:24 -0400

    clean up

--- models/linear-regression.py
+++ models/linear-regression.py
@@ -6,47 +6,30 @@ print(iris.head(10));
 petalWidth_petalLength = iris[["petal.width", "petal.length"]];
 print(petalWidth_petalLength.head(10));
 
-
-
 # y = mx + b
-x = petalWidth_petalLength["petal.length"];
-y = petalWidth_petalLength["petal.width"];
 n = len(petalWidth_petalLength);
-
 sumOfX = 0;
 sumOfX2 = 0;
 sumOfY = 0;
 sumOfY2 = 0;
 sumOfXY = 0;
 
-
 for i, row in petalWidth_petalLength.iterrows():
-    print("length: ", row['petal.length']);
-    x = row['petal.length'];
-    sumOfX += x;
-    sumOfX2 += (x * x);
-
-    print("width: ", row['petal.width']);
-    y = row['petal.width'];
-    sumOfY += y;
-    sumOfY2 += (y * y);
-
-    sumOfXY += (x * y);
+    x_train = row['petal.length'];
+    sumOfX += x_train;
+    sumOfX2 += (x_train * x_train);
+    y_train = row['petal.width'];
+    sumOfY += y_train;
+    sumOfY2 += (y_train * y_train);
+    sumOfXY += (x_train * y_train);
 
 m = ((n * (sumOfXY)) - (sumOfX * sumOfY))/((n * (sumOfX2)) - (sumOfX * sumOfX));
 b = (sumOfY - (m * (sumOfX)))/n;
 
-print("x: ", x);
-print("y: ", y);
-print("sumOfX: ", sumOfX);
-print("sumOfX2: ", sumOfX2);
-print("sumOfY: ", sumOfY);
-print("sumOfY2: ", sumOfY2);
-print("sumOfXY: ", sumOfXY);
-print("m: ", m);
-print("b: ", b);
+print("Slope: ", m);
+print("Y-intercept: ", b);
 
-input = input('Predict Y given X');
+input = input('Predict Y given X: ');
 
 def linearRegression(X):
     Y = ((m * X) + b);

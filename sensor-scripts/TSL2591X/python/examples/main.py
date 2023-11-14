639cd821ccdb8d7154ac06d60143536ff77db1bd
illenafnayr <ryanfanelli@greenmountainpower.com>
2023-11-13 20:44:14 -0500

    add time stamp

--- sensor-scripts/TSL2591X/python/examples/main.py
+++ sensor-scripts/TSL2591X/python/examples/main.py
@@ -2,6 +2,7 @@ import time
 import sys
 import os
 import csv
+from datetime import datetime  # Import the datetime module
 
 libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
 if os.path.exists(libdir):
@@ -19,16 +20,20 @@ csv_file_path = 'sensor_data.csv'
 try:
     with open(csv_file_path, mode='w', newline='') as csvfile:
         csv_writer = csv.writer(csvfile)
-        csv_writer.writerow(['Lux', 'Infrared', 'Visible', 'Full Spectrum'])
+        csv_writer.writerow(['Timestamp', 'Lux', 'Infrared', 'Visible', 'Full Spectrum'])
 
         while True:
             lux = sensor.Lux
             infrared = sensor.Read_Infrared
             visible = sensor.Read_Visible
             full_spectrum = sensor.Read_FullSpectrum
-            row = [lux, infrared, visible, full_spectrum]
 
-            print('Lux: %d, Infrared light: %d, Visible light: %d, Full spectrum (IR + visible) light: %d' % tuple(row))
+            # Get the current timestamp
+            timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+            row = [timestamp, lux, infrared, visible, full_spectrum]
+
+            print('Timestamp: %s, Lux: %d, Infrared light: %d, Visible light: %d, Full spectrum (IR + visible) light: %d' % tuple(row))
             csv_writer.writerow(row)
 
             time.sleep(1)

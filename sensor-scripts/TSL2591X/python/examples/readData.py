e3c033380d6534f4808459390588aa22ee4d893a
illenafnayr <ryanfanelli@greenmountainpower.com>
2023-11-14 00:22:33 -0500

    readData.py to read gaia-sensor-data-updated.csv

---
+++ sensor-scripts/TSL2591X/python/examples/readData.py
@@ -0,0 +1,22 @@
+import boto3
+import pandas as pd
+from io import StringIO
+
+def read_merged_csv_from_s3(aws_access_key_id, aws_secret_access_key, aws_region, s3_bucket_name, s3_object_key):
+    s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, region_name=aws_region)
+
+    csv_content = s3.get_object(Bucket=s3_bucket_name, Key=s3_object_key)['Body'].read().decode('utf-8')
+
+    df = pd.read_csv(StringIO(csv_content))
+
+    return df
+
+aws_access_key_id = ''
+aws_secret_access_key = ''
+aws_region = 'us-east-1'
+s3_bucket_name = 'gaia-sensor-data'
+s3_object_key = 'gaia-sensor-data-updated.csv'
+
+merged_df = read_merged_csv_from_s3(aws_access_key_id, aws_secret_access_key, aws_region, s3_bucket_name, s3_object_key)
+
+print(merged_df)

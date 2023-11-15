e27d35be368de2eeb7bc49116dbbf1765fbc17a0
illenafnayr <illenafnayr@gmail.com>
2023-11-14 23:14:49 -0500

    update batch.py

--- batch/batch.py
+++ batch/batch.py
@@ -1,5 +1,7 @@
-from ..sensorscripts.DataLogger import DataLogger
-from ..sensorscripts.TSL2591X.TSLX2591XDataLogger import LightSensorDataLogger
+import time
+from sensorscripts.DataLogger import DataLogger
+from sensorscripts.TSL2591X.TSLX2591XDataLogger import LightSensorDataLogger
+
 custom_header = ['Timestamp', 'Lux', 'Infrared', 'Visible', 'FullSpectrum']
 logger = DataLogger(LightSensorDataLogger, csv_file_path='light-sensor-data.csv', header=custom_header)
 logger.log_sensor_data()

dfdc2ff3a4e20eca592a8859496549e5ec1ad8ae
illenafnayr <illenafnayr@gmail.com>
2023-11-14 23:39:30 -0500

    update batch.py

--- batch/batch.py
+++ batch/batch.py
@@ -1,12 +1,21 @@
+# batch.py
 import time
-from ..sensorscripts.DataLogger import DataLogger
-from ..sensorscripts.TSL2591X.TSLX2591XDataLogger import LightSensorDataLogger
+import sys
+import os
+
+# Add the project root to the Python path
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, project_root)
+
+from sensorscripts.DataLogger import DataLogger
+from sensorscripts.TSL2591X.TSLX2591XDataLogger import LightSensorDataLogger
 
 custom_header = ['Timestamp', 'Lux', 'Infrared', 'Visible', 'FullSpectrum']
 logger = DataLogger(LightSensorDataLogger, csv_file_path='light-sensor-data.csv', header=custom_header)
 logger.log_sensor_data()
-time.sleep(3)
+time.sleep(5)
 logger.log_sensor_data()
-time.sleep(3)
+time.sleep(5)
 logger.log_sensor_data()
-time.sleep(3)
+time.sleep(5)
+logger.log_sensor_data()

82179675f03ba42689e1c49f0fc72b71a742452b
illenafnayr <illenafnayr@gmail.com>
2023-11-14 22:38:32 -0500

    timeout test

--- sensorscripts/DataLogger.py
+++ sensorscripts/DataLogger.py
@@ -1,4 +1,6 @@
+import time
 import csv
+
 from TSL2591X.TSLX2591XDataLogger import LightSensorDataLogger
 
 class DataLogger:
@@ -13,34 +15,21 @@ class DataLogger:
             csv_writer.writerow(self.header)
 
     def log_sensor_data(self):
-        try:
-            with open(self.csv_file_path, mode='a', newline='') as csvfile:
-                csv_writer = csv.writer(csvfile)
-                # Create an instance of LightSensorDataLogger
-                sensor_instance = self.sensorLogger()
-                # Get the current timestamp
-                # timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-                # Create a row of sensor data
-                row = sensor_instance.writeRow()
+        while True:
+            try:
+                with open(self.csv_file_path, mode='a', newline='') as csvfile:
+                    csv_writer = csv.writer(csvfile)
+                    sensor_instance = self.sensorLogger()
+                    # timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                    row = sensor_instance.writeRow()
+                    csv_writer.writerow(row)
 
-                # Log the data to the CSV file
-                csv_writer.writerow(row)
-        except KeyboardInterrupt:
-            print("ctrl + c:")
-            # Handle any necessary cleanup or logging when KeyboardInterrupt occurs
+            except KeyboardInterrupt:
+                print("ctrl + c:")
 
-# Example usage:
-# Assuming the SensorDataLogger is initialized somewhere in your main script
-# and you have sensor data to log
-# lux_value = 100  # Replace with actual Lux value
-# infrared_value = 50  # Replace with actual Infrared value
-# visible_value = 75  # Replace with actual Visible value
-# full_spectrum_value = 125  # Replace with actual FullSpectrum value
 
-# Dynamic CSV file path and header
-# custom_csv_file_path = 'custom_sensor_data.csv'
 custom_header = ['Timestamp', 'Lux', 'Infrared', 'Visible', 'FullSpectrum']
-
-# Create an instance of the SensorDataLogger with dynamic parameters
 logger = DataLogger(LightSensorDataLogger, csv_file_path='light-sensor-data.csv', header=custom_header)
-logger.log_sensor_data()
+while True:
+    logger.log_sensor_data(3)
+    time.sleep(3)

cacbab4536ad5659384be45b61a7b2402c86c0c8
RyanFanelli <ryan.fanelli@greenmountainpower.com>
2023-11-17 19:51:38 -0500

    update DataLogger

--- sensorscripts/DataLogger.py
+++ sensorscripts/DataLogger.py
@@ -1,6 +1,5 @@
 import csv
 from collections import OrderedDict
-from datetime import datetime
 import logging
 
 class DataLogger:
@@ -29,29 +28,24 @@ class DataLogger:
             with open(self.csv_file, 'r') as file:
                 reader = csv.DictReader(file)
                 for row in reader:
-                    if 'timestamp' in row:
-                        timestamp = row['timestamp']
-                        data[timestamp] = row
-                    else:
-                        logging.warning(f"Skipping row without timestamp: {row}")
+                    data[row['timestamp']] = row
         except FileNotFoundError:
             pass
         return data
+
     def add_data(self):
         new_data = self.sensor_logger.writeRow()
 
         # If headers are not set, use the headers from sensor_logger
-        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-
         # Convert new_data to a list of strings
         new_data_strings = [str(item) for item in new_data]
 
         # Create a new row dictionary
-        new_row = {'timestamp': timestamp, **dict(zip(self.headers, new_data_strings))}
+        new_row = dict(zip(self.headers, new_data_strings))
         print(new_row)
 
         # Append the new row to the data dictionary
-        self.data[timestamp] = new_row
+        self.data[len(self.data) + 1] = new_row  # Using a numeric key instead of timestamp
 
         # Save to CSV without using with block
         self.save_to_csv()
@@ -65,27 +59,8 @@ class DataLogger:
             writer.writerow(header_row)
 
             # Write data rows
-            for timestamp, row in self.data.items():
-                if isinstance(row, dict):
-                    # Extract values including a default value for missing keys
-                    values = [row.get(key, '') for key in self.headers]
-                    # Write the row with the timestamp
-                    writer.writerow([timestamp] + values)
-                else:
-                    print(f"Warning: Unexpected data format for timestamp {timestamp}: {row}")
-
-            with open(self.csv_file, 'w', newline='') as file:
-                writer = csv.writer(file)
-
-                # Write header row
-                header_row = self.headers
-                writer.writerow(header_row)
-
-
-    def add_timestamp(self):
-        current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        for timestamp in self.data:
-            if isinstance(self.data[timestamp], dict):
-                self.data[timestamp]['timestamp'] = current_time
-            else:
-                print(f"Warning: Unexpected data format for timestamp {timestamp}: {self.data[timestamp]}")
+            for _, row in self.data.items():
+                # Extract values including a default value for missing keys
+                values = [row.get(key, '') for key in self.headers]
+                # Write the row
+                writer.writerow(values)
